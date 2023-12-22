85de3aaa011cc1ed860a257b9040c1c83ab4f9df
Emilion <emiljadersten@gmail.com>
2023-12-22 14:48:37 +0100

    Add basic functions to python code

--- run.py
+++ run.py
@@ -1,1 +1,27 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+def initialize_game():
+    """
+    Initialize game state and relevant variables
+    """
+
+def create_questions():
+    """
+    Load questions from file
+    """
+
+def show_high_score():
+    """
+    Show list of high scores
+    """
+
+def start_round():
+    """
+    Begin the game
+    """
+
+def ask_question():
+    """
+    Present the next question and the alternatives for answers
+    """
