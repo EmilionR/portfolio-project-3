08c910bd4a7cce9ce5e01d0141b4b27a49d37617
Emilion <emiljadersten@gmail.com>
2023-12-22 14:53:05 +0100

    Add the rest of the  core  functions to python code

--- run.py
+++ run.py
@@ -25,3 +25,29 @@ def ask_question():
     """
     Present the next question and the alternatives for answers
     """
+
+def player_answer():
+    """
+    Player inputs their answer
+    """
+
+def check_if_correct():
+    """
+    Check if the answer given is correct,
+    display result and award points if correct
+    """
+
+def game_over():
+    """
+    Calculate final score and check if hi-score is achieved
+    """
+
+def update_high_score():
+    """
+    Update high score list
+    """
+
+def restart_game():
+    """
+    Start a new round of the quiz
+    """
